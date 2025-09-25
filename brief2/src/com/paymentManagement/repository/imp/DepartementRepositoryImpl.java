aacd7bbd5bc7917d4804fed54a7d11bebc99812b
youness <youness.ouahmane4@gmail.com>
2025-09-25 16:02:45 +0100

    implmeneted findById, findbyid,findall

--- brief2/src/com/paymentManagement/repository/imp/DepartementRepositoryImpl.java
+++ brief2/src/com/paymentManagement/repository/imp/DepartementRepositoryImpl.java
@@ -4,9 +4,13 @@ import com.paymentManagement.config.database.DatabaseConnection;
 import com.paymentManagement.model.entity.Departement;
 import com.paymentManagement.repository.interfaces.DepartementRepository;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DepartementRepositoryImpl implements DepartementRepository {
     @Override
@@ -19,7 +23,87 @@ public class DepartementRepositoryImpl implements DepartementRepository {
             statement.executeUpdate();
             statement.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("error saving departement" + e.getMessage());
         }
     }
+
+    @Override
+    public Departement findById(Integer id) {
+        try{
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            String sql = "select * from departement where departement.id = ?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+
+            if(resultSet.next())
+            {
+                Departement dep = new Departement(resultSet.getInt("id_departement"), resultSet.getString("nom"), resultSet.getInt("responsable_id"));
+                resultSet.close();
+                stmt.close();
+                return dep;
+            }
+            resultSet.close();
+            stmt.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error finding departement: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Departement> findAll() {
+        List<Departement> departements = new ArrayList<>();
+        try{
+
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            String sql = "select * from departements";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultSet = stmt.getResultSet();
+            while(resultSet.next())
+            {
+                Departement departement = new Departement(resultSet.getInt("id_departement"), resultSet.getString("nom"), resultSet.getInt("id_departement"));
+                departements.add(departement);
+            }
+            resultSet.close();
+            stmt.close();
+            }catch (SQLException e){
+                System.out.println("Error getting deps" + e.getMessage());
+            }
+        return departements;
+
+        }
+
+    public Departement findByNom(String nom) {
+        try {
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            String sql = "SELECT * FROM departements WHERE nom = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, nom);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                Departement departement = new Departement();
+                departement.setIdDepartement(resultSet.getInt("id_departement"));
+                departement.setNom(resultSet.getString("nom"));
+
+                resultSet.close();
+                statement.close();
+                return departement;
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error finding departement by name: " + e.getMessage());
+        }
+
+        return null;
+    }
 }
+
+
