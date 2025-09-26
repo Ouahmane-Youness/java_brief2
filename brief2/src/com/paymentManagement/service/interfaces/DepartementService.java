b0a6b2d74974edd26f2efabe42cdb3b3a04410c1
youness <youness.ouahmane4@gmail.com>
2025-09-26 16:14:31 +0100

    created department service

---
+++ brief2/src/com/paymentManagement/service/interfaces/DepartementService.java
@@ -0,0 +1,24 @@
+package com.paymentManagement.service.interfaces;
+
+import com.paymentManagement.model.entity.Agent;
+import com.paymentManagement.model.entity.Departement;
+
+import java.util.List;
+
+public interface DepartementService {
+    Departement createDepartement(String nom);
+    Departement findDepartementById(Integer id);
+    Departement findDepartementByNom(String nom);
+    List<Departement> getAllDepartements();
+    void updateDepartement(Departement departement);
+    boolean deleteDepartement(Integer id);
+
+    // Business logic operations
+    boolean assignManager(Integer departementId, Integer managerId);
+    boolean removeManager(Integer departementId);
+    List<Agent> getAgentsInDepartement(Integer departementId);
+    int getAgentCount(Integer departementId);
+    boolean isDepartementNameAvailable(String nom);
+
+    boolean canAgentManageDepartement(Agent agent);
+}
