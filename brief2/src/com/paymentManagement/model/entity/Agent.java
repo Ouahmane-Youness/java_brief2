6b165a59ee3381f8dfdb6f18864aba9d3811f68b
youness <youness.ouahmane4@gmail.com>
2025-09-26 10:03:47 +0100

    add getters and setters for Agent entity

--- brief2/src/com/paymentManagement/model/entity/Agent.java
+++ brief2/src/com/paymentManagement/model/entity/Agent.java
@@ -12,7 +12,39 @@ public class Agent extends Personne {
         super(nom, prenom, email, motDePasse);
         this.idAgent = idAgent;
         this.departement = departement;
-        this.idDepartementId = idDepartementId;
+        this.idDepartement = idDepartementId;
+        this.paiements = new ArrayList<>();
+    }
+
+    public Integer getIdAgent() {
+        return idAgent;
+    }
+
+    public void setIdAgent(Integer idAgent) {
+        this.idAgent = idAgent;
+    }
+
+    public Departement getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(Departement departement) {
+        this.departement = departement;
+    }
+
+    public Integer getIdDepartementId() {
+        return idDepartement;
+    }
+
+    public void setIdDepartementId(Integer idDepartementId) {
+        this.idDepartement = idDepartementId;
+    }
+
+    public List<Paiement> getPaiements() {
+        return paiements;
+    }
+
+    public void setPaiements(List<Paiement> paiements) {
         this.paiements = paiements;
     }
 }
