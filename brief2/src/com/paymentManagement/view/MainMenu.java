97679936bd439d2621721edec5ae10e7a2c8319e
youness <youness.ouahmane4@gmail.com>
2025-09-29 16:58:25 +0100

    created main menu class

---
+++ brief2/src/com/paymentManagement/view/MainMenu.java
@@ -0,0 +1,16 @@
+package com.paymentManagement.view;
+
+import com.paymentManagement.service.impl.AgentServiceImpl;
+import com.paymentManagement.service.interfaces.AgentService;
+
+
+public class MainMenu {
+    private final AgentServiceImpl agentService;
+    private final InputHandler inputHandler;
+
+    public MainMenu()
+    {
+        this.agentService = new AgentServiceImpl();
+        this.inputHandler = new InputHangler();
+    }
+}
